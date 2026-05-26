35d53f4479b8e5e755ef875ea940a5a6883ddcdb
shifa-developer20058 <shifafathima20058@gmail.com>
2026-05-26 18:17:58 +0630

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Samp from "./Samp"
+function App() {
+  return (
+    <div className="text-9xl italic font-bold text-blue-600
+    p-10 text-center">
+     <h1> Tailwind Working</h1>
+<Samp/>
+
+
+      </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Samp.jsx
@@ -0,0 +1,9 @@
+function Samp() {
+    return (
+        <h1 className="text-5x1 text-red-500 mt-10">
+            Hello From Samp Component
+        </h1>
+    )
+}
+
+export default Samp
